5d3fa5647c24d770d335513cbac480dc65a2eb2d
MeteorGroup <112450565+MeteorGroupSite@users.noreply.github.com>
2024-11-22 12:49:12 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// JavaScript for contact button
+document.getElementById('contactButton').addEventListener('click', () => {
+    alert('Thanks for your interest! https://docs.google.com/forms/d/e/1FAIpQLSeIqdfer_S2zh0sIOV4yoED92hG5KJMjv74uaaYAFZdOjOH9g/viewform');
+});
+
+// JavaScript for footer contact link
+document.getElementById('contactLink').addEventListener('click', (e) => {
+    e.preventDefault();
+    alert('Reach us at: https://docs.google.com/forms/d/e/1FAIpQLSeIqdfer_S2zh0sIOV4yoED92hG5KJMjv74uaaYAFZdOjOH9g/viewform');
+});
